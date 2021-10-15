92b615979e2dcc3aca115c84660a3baaf5d89622
Alexandre <alexandre-boulanger@bbox.fr>
2021-10-15 11:59:50 +0200

    Finalisation du constructeur de la classe Donnee à partir du chemin d'accès d'un fichier donné

--- src/main/java/edu/centralenantes/pappl/Donnee.java
+++ src/main/java/edu/centralenantes/pappl/Donnee.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.swing.JFileChooser;
@@ -17,16 +18,17 @@ import javax.swing.JFileChooser;
  * @author Boulanger
  */
 public class Donnee {
-    private String nom;
-    private String path;
-    private String extension;
-    private long dateCrea;
-    private long dateOuvert;
-    private int freqModif;
-    private int freqOuvert;
-    private int nbNom;
-    private float taille;
-    private float score;
+    private String nom; // Nom du Fichier
+    private String path; // Path du fichier
+    private String extension; // Extension du fichier (pdf, png,..)
+    private FileTime dateCrea; // date de creation du fichier
+    private FileTime dateOuvert; // date de dernière ouverture du fichier
+    private FileTime dateModif; // date de dernière modif du fichier
+    private int freqModif; // fréquence de modif du fichier
+    private int freqOuvert; // fréquence d'ouverture du fichier
+    private int nbNom; // nombre de fois que le Nom est "répété"
+    private long taille; // taille du fichier en byte
+    private float score; // score total du fichier
 
     public String getNom() {
         return nom;
@@ -52,19 +54,19 @@ public class Donnee {
         this.extension = extension;
     }
 
-    public long getDateCrea() {
+    public FileTime getDateCrea() {
         return dateCrea;
     }
 
-    public void setDateCrea(long dateCrea) {
+    public void setDateCrea(FileTime dateCrea) {
         this.dateCrea = dateCrea;
     }
 
-    public long getDateOuvert() {
+    public FileTime getDateOuvert() {
         return dateOuvert;
     }
 
-    public void setDateOuvert(long dateOuvert) {
+    public void setDateOuvert(FileTime dateOuvert) {
         this.dateOuvert = dateOuvert;
     }
 
@@ -92,11 +94,11 @@ public class Donnee {
         this.nbNom = nbNom;
     }
 
-    public float getTaille() {
+    public long getTaille() {
         return taille;
     }
 
-    public void setTaille(float taille) {
+    public void setTaille(long taille) {
         this.taille = taille;
     }
 
@@ -108,12 +110,30 @@ public class Donnee {
         this.score = score;
     }
     
+    /*
+    * Constructeur de la Classe Donnee créant une Donnee à partir d'un path lié à un vrai fichier
+    */
     public Donnee(String _path) throws IOException{
         this.path=_path;
         File file= new File(path);
         BasicFileAttributes attr = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
         this.nom=file.getName();
-        this.dateCrea=file.lastModified();
-        this.dateCrea=file.creationTime();
+        this.dateModif=attr.lastModifiedTime();
+        this.dateCrea=attr.creationTime();
+        this.dateOuvert=attr.lastAccessTime();
+        this.taille= attr.size();
+
+        int pos = path.lastIndexOf(".");// On recherhce le dernier . dans le fichierqui va correspondre au début de l'extension
+        if (pos > -1) {
+            this.extension=path.substring(pos);// On supprime tous les éléments du String précédent le point trouvé
+        }
+        else{
+            this.extension=null;
+        }
+        this.nbNom=0;
+        this.freqModif=0;
+        this.freqOuvert=0;
+        this.score=0;
     }
+    
 }
