5c788ff69db9ad7d06b55d545c223f912d8c8e0c
Anne-So <anne-sophie.jourlin@eleves.ec-nantes.fr>
2021-10-20 15:13:26 +0200

    getteurs setteurs

--- src/main/java/edu/centralenantes/pappl/GestionScore.java
+++ src/main/java/edu/centralenantes/pappl/GestionScore.java
@@ -5,10 +5,34 @@
  */
 package edu.centralenantes.pappl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author asjou
  */
 public class GestionScore {
+    private List<String> path;
+    private List<Donnee> donnees;
+    
+    public GestionScore() {
+        this.path = new ArrayList<String>();
+        //Le chemin par défaut est celui du dossier téléchargements
+        path.add("C:\\Downloads");
+    }
+    
+    public GestionScore(List<String> path) {
+        this.path = path;
+    }    
+
+    public List<String> getPath() {
+        return path;
+    }
+
+    public void setPath(List<String> path) {
+        this.path = path;
+    }
+    
     
 }
