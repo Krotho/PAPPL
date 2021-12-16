41ee018a8f7d301ef47fa7ec171bf8b94d2c40d2
Boulanger <alexandre-boulanger@bbox.fr>
2021-12-16 14:13:04 +0100

    Suppression du fichier config et ajout des licences

--- src/main/java/edu/centralenantes/pappl/InterfaceGraphiqueExtensions.java
+++ src/main/java/edu/centralenantes/pappl/InterfaceGraphiqueExtensions.java
@@ -1,7 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+*PAPPL outil de conseil à l'archivage et à la suppression de fichiers sur une machine personnelle
+*Copyright (C) 2021  Boulanger & Jourlin, Ecole Centrale de Nantes
+*
+*This library is free software; you can redistribute it and/or
+*modify it under the terms of the GNU Lesser General Public
+*License as published by the Free Software Foundation; either
+*version 2.1 of the License, or (at your option) any later version.
+*
+*This library is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*Lesser General Public License for more details.
+*
+*You should have received a copy of the GNU Lesser General Public
+*License along with this library; if not, write to the Free Software
+*Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+*USA
  */
 package edu.centralenantes.pappl;
 
